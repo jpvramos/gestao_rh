3dece6b4dc1a7ee847df48cacf6ec3e71c41c019
jpramos <joaopaulovieiraramos@gmail.com>
2021-07-18 17:55:03 -0300

    feat: Apps Departamentos

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.departamentos',
     'apps.empresas',
     'apps.funcionarios',
 ]
