33d7553077ffee7d642db3543244e1ee9bb731e4
jpramos <joaopaulovieiraramos@gmail.com>
2021-07-18 18:58:07 -0300

    feat: create relationships one to one and many to one

--- apps/registro_hora_extra/models.py
+++ apps/registro_hora_extra/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from apps.funcionarios.models import Funcionario
 
 class RegistroHoraExtra(models.Model):
     motivo = models.CharField(max_length=100)
+    funcionarios = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
     
     def __str__(self):
         return self.motivo
